f864b61adc671a416fe8ef907a8c446eb7e4389b
KatNat <48943466+KatNat@users.noreply.github.com>
2019-03-30 00:05:11 +0700

    Update QInt.h

--- QInt.h
+++ QInt.h
@@ -2,14 +2,32 @@
 #define _QINT_H_
 
 #include "binary.h"
+#include <sstream>
+
 
 class QInt {
 private:
-    binary n;
+	binary n;
+
+	void _string_div_two(std::string&);
+	int _multiply(int, int[], int);
+	std::string _power(int, int);
+	std::string _add_string(std::string, std::string);
 
 public:
-    QInt();
-    QInt(const int&);
-}
+	QInt();
+	QInt(const int&);
+
+	void scan_QInt(const std::string, const int);
+	std::string print_QInt(const int);
+
+	std::string bin_to_dec(std::string);
+	std::string bin_to_hex(std::string);
+	std::string dec_to_bin(std::string);
+	std::string dec_to_hex(std::string);
+	std::string hex_to_bin(std::string);
+	std::string hex_to_dec(std::string);
+
+};
 
 #endif // _QINT_H_
